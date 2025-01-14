32722f837ed88f7a5a0f74e0365998a5404be570
talhausman-it <tu6049344@gmail.com>
2025-01-14 22:38:48 +0500

    Add files via upload

---
+++ output and input.py
@@ -0,0 +1,20 @@
+print("name")
+#separating with comma
+print("k","f","g",sep="")
+print("2","4","19",sep="-")
+
+#f string
+name = 'talha'
+age = 23
+print(f"Hello, My name is {name} and I'm {age} years old.")
+
+
+#taking a two input at a time
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+#taking athree input at time
+x , y, z = input("enter three values:") .split()
+print("total number:", z)
+print("number of girls:", y)
+print("number of boys:", x)
