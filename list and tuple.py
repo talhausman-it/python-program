bd91d6f2840817535eab9e9fd205d7752e4e065a
talhausman-it <tu6049344@gmail.com>
2025-01-16 02:02:53 +0500

    Add files via upload

---
+++ list and tuple.py
@@ -0,0 +1,36 @@
+student = ["talha", 88, 17, "pakpattan"]
+print(student)
+print(student[1])
+#we change value in list
+student[1] = "usman"
+print(student)
+#list slicing
+marks = [92, 88, 77, 66, 45, 34]
+print(marks[1:4])
+#negative index
+marks = [55, 67, 45, 77, 87,96,34]
+print(marks[-6:-2])
+#list method
+#append method in whic we add
+list = [2, 1, 6, 7]
+list.append(9, )
+print(list)
+#sort list
+list = [2, 1, 6, 7]
+print(list.sort())
+print(list)
+#decending sort
+list = [2, 1, 6, 7]
+print(list.sort(reverse=True))
+print(list)
+#reverse method
+list = ['a','b','c','d','e']
+list.reverse()
+print(list)
+#insert method
+list = [2, 1, 6, 7]
+list.insert(1,9 )
+print(list)
+#tupe are mostly similar as compare to list
+tup = (2,3,4,5)
+print(tup)
