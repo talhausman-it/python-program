d16bac2350f79ef45bbdf492806c225544d18df0
talhausman-it <tu6049344@gmail.com>
2025-06-14 16:05:50 +0500

    Add files via upload

---
+++ if esle.py
@@ -0,0 +1,9 @@
+temp = float(input("enter the tempreater :"))
+if temp==0:
+    print("it is freezing")
+elif temp==18:
+    print("it is normal")
+elif temp<=30:
+    print("it is to normal")
+else:
+    print("it is not normal")
